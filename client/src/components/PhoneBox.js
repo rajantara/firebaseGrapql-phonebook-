c20fce2f1c52cb73f645ad30bfcf341d18b6a128
rajantara <rajanpensas@gmail.com>
2020-09-03 01:31:52 +0800

    view done

--- client/src/components/PhoneBox.js
+++ client/src/components/PhoneBox.js
@@ -6,6 +6,15 @@ export default class PhoneBox extends Component {
         return (
             <div className="container-fluid my-5">
                 <div className="container card">
+                <div className="card-header text-center">
+                        <h1> <i className="fa fa-address-book"></i> Phones Book Apps</h1>
+                    </div>
+                    <div className="card-body">
+                       
+                    </div>
+                    <div className="card-footer text-center">
+                    <i className="far fa-copyright"></i> Created by La ode rajantara
+                    </div>
 
                 </div>
 
