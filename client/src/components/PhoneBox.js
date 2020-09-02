2ce1df3115b4c02909213364499befcc6ec0554e
rajantara <rajanpensas@gmail.com>
2020-09-03 01:18:03 +0800

    add data phonebox

---
+++ client/src/components/PhoneBox.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+export default class PhoneBox extends Component {
+    render () {
+        return (
+            <div className="container-fluid my-5">
+                <div className="container card">
+
+                </div>
+
+            </div>
+        )
+    }
+}
